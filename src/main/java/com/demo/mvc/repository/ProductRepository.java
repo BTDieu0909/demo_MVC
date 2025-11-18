4c06df0a34a537752dc71d4210a84b0f3fd57126
BTD <buithidieu2004gl@gmail.com>
2025-11-18 22:35:25 +0700

    Bo sung thêm

--- src/main/java/com/demo/mvc/repository/ProductRepository.java
+++ src/main/java/com/demo/mvc/repository/ProductRepository.java
@@ -2,6 +2,9 @@ package com.demo.mvc.repository;
 
 import com.demo.mvc.entity.Product;
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
